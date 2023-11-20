3b6f5957c6d94b761ec12c77e527c2fbcd1f761e
Bruno Alves <boteistem@gmail.com>
2023-11-20 13:14:37 -0300

    feat: login cliente

---
+++ app/lib/login/Customer.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Retroflix\lib\login;
+use Retroflix\interfaces\LoginInterface;
+use Retroflix\models\cliente\ClienteModel;
+use Retroflix\Entity\cliente\Cliente;
+class Customer implements LoginInterface{
+
+    protected string $username;
+    protected string $password;
+    public function __construct(){
+        @session_start();
+    }
+    public function login(string $username, string $password){
+        $this->username = $username;
+        $this->password = $password;
+        
+        $cliente = new Cliente();
+        $cliente->email = $this->username;
+        $cliente->senha = $this->password;
+    
+        $user = (new ClienteModel)->login($cliente);
+       
+        if (count($user) > 0) {
+            $this->setLogin($user);
+            $this->redirect("/filmes/");
+        }else{
+            $this->redirect("/cliente/?login=false");
+        }
+
+        
+    }   
+    public function setLogin(array $data = []){
+    
+        $_SESSION["customer"]["login"] = true;
+        $_SESSION["customer"]["data"] = $data; 
+    }
+    public function logout(){
+
+        session_destroy();
+        $this->redirect();
+
+    }
+
+    public function isLoggedIn() : bool{
+        if ( isset($_SESSION["customer"]["login"]) && $_SESSION["customer"]["login"] == true){
+            return true;
+        }
+        return false;
+    }
+    public function redirect(string $url = ""){
+
+        if (empty($url)){
+            header("Location: /cliente/");
+        }else{
+            header("Location: $url");
+        }
+
+    }
+}
--- public/cliente/index.php
+++ public/cliente/index.php
@@ -1,25 +1,93 @@
 <?php
 
-require __DIR__.'/../../vendor/autoload.php';
-require __DIR__.'/../../app/config/config.php';
+require __DIR__."/../../vendor/autoload.php";
+require __DIR__."/../../app/config/config.php";
+use Retroflix\lib\login\Customer;
+
+if (isset($_POST["login"])) {
+
+  $username = $_POST["email"];
+  $password = $_POST["password"];
+   (new Customer())->login($username, $password);
 
 
-//simples renderização de dados da tabela
+}
+if (isset($_GET['login'])){
+  $msg = true;
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
 
-$ClientModel = new Retroflix\models\cliente\ClienteModel();
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin - Retroflix</title>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+</head>
+<body>
 
-$cliente = new Retroflix\Entity\cliente\Cliente();
+<section class="vh-100">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-6 text-black">
+
+        <div class="px-5 ms-xl-4">
+          <i class="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" style="color: #709085;"></i>
+          <span class="h1 fw-bold mb-0">Logo</span>
+        </div>
+
+        <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
+
+          <form style="width: 23rem;">
+
+            <h3 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Log in</h3>
+
+            <div class="form-outline mb-4">
+              <input type="email" id="form2Example18" class="form-control form-control-lg" />
+              <label class="form-label" for="form2Example18">Email address</label>
+            </div>
+
+            <div class="form-outline mb-4">
+              <input type="password" id="form2Example28" class="form-control form-control-lg" />
+              <label class="form-label" for="form2Example28">Password</label>
+            </div>
+
+            <div class="pt-1 mb-4">
+              <button class="btn btn-info btn-lg btn-block" type="button">Login</button>
+            </div>
+
+            <p class="small mb-5 pb-lg-2"><a class="text-muted" href="#!">Forgot password?</a></p>
+            <p>Don't have an account? <a href="#!" class="link-info">Register here</a></p>
+
+          </form>
+
+        </div>
+
+      </div>
+      <div class="col-sm-6 px-0 d-none d-sm-block">
+        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img3.webp"
+          alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: left;">
+      </div>
+    </div>
+  </div>
+</section>
 
 
-$cliente->nome = "Gabriel";
-$cliente->valor = 451;
-$cliente->sobrenome = "aaaa";
-//$banco = new Retroflix\db\DB();
-//$clientes = $banco->execute("SELECT * FROM teste");
-//var_dump($clientes->fetchAll());
+<script>
 
-$ClientModel->create($cliente);
-//var_dump( $ClientModel->find($cliente)  );
+  function loginError(){
+    Swal.fire({
+    icon: "error",
+    title: "Login Inválido",
+    text: "Tente novamente!"
+    });
+  }
 
-
-//var_dump($ClientModel->getAll());
+</script>
+</body>
