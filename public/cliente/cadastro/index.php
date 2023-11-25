135ef9ff548e2d1caa0ffd6872d4812b32827740
Bruno Alves <boteistem@gmail.com>
2023-11-25 12:18:51 -0300

    fix: redirect in customer register

--- public/cliente/cadastro/index.php
+++ public/cliente/cadastro/index.php
@@ -32,23 +32,32 @@
         $result = $clienteModel->create($cliente);
 
         if ($result){
-            header("Location: /admin/dashboard/clientes/");
+            header("Location: /cliente/");
         }
     }
 ?>
-
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+</head>
 <body>
     <!-- Body -->
     <div class="container-fluid">
         <div class="row flex-nowrap">
             <!-- Chamando a Side-Bar do sistema -->
             <?php 
-                require "../../admin/dashboard/componentes/side-bar.php";
+                //require "../../admin/dashboard/componentes/side-bar.php";
             ?>
 
             <!-- Conteúdo do site -->
             <div class="col-auto col-md-9 ms-5 mt-4 bg-light py-3">
-                <h2>Clientes</h2>
+                <h2>Cadastre-se</h2>
 
                 <!-- Breadcrumb -->
                 <nav class="mt-4 mb-2" style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
